902cfccb3c14d0b7ccb25f7eb0d771e2f47408be
nicolay-g <74829350+nicolay-g@users.noreply.github.com>
2024-03-30 16:20:48 +0200

    resources directory commit

--- src/test/java/ui_tests/SkilloSiteTests.java
+++ src/test/java/ui_tests/SkilloSiteTests.java
@@ -1,5 +1,12 @@
 package ui_tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SkilloSiteTests extends TestObject {
 
+    @Test
+    public void initialTest() {
+        Assert.assertTrue(true);
+    }
 }
