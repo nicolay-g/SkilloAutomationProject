db672eed6a8a05619aad144bf56bffb4e15e76b9
nicolay-g <74829350+nicolay-g@users.noreply.github.com>
2024-03-31 09:18:35 +0300

    Added ability to save screenshots and delete them before the next execution of the tests

--- src/test/java/ui_tests/TestObject.java
+++ src/test/java/ui_tests/TestObject.java
@@ -1,12 +1,18 @@
 package ui_tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,13 +20,14 @@ public class TestObject {
     public static final String HOME_URL = "http://training.skillo-bg.com:4200/posts/all";
     public static final String TEST_RESOURCES_DIR = "src\\test\\resources\\";
     public static final String DOWNLOAD_DIR = TEST_RESOURCES_DIR.concat("download\\");
+    public static final String SCREENSHOTS_DIR = TEST_RESOURCES_DIR.concat("screenshots\\");
     public WebDriver getWebDriver() {
         return webDriver;
     }
     private WebDriver webDriver;
     @BeforeSuite
-    protected final void setupTestSuite() {
-
+    protected final void setupTestSuite() throws IOException {
+        cleanDirectory(SCREENSHOTS_DIR);
         WebDriverManager.chromedriver().setup();
     }
 
@@ -32,6 +39,7 @@ public class TestObject {
     }
     @AfterMethod
     protected final void tearDownTest(ITestResult testResult) {
+        takeScreenshot(testResult);
         quitDriver();
     }
     @AfterSuite
@@ -58,5 +66,31 @@ public class TestObject {
         chromeOptions.addArguments("disable-popup-blocking");
 
         return chromeOptions;
+    }
+
+    private void takeScreenshot(ITestResult testResult) {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            String testName = testResult.getName();
+            try {
+                TakesScreenshot takesScreenshot = (TakesScreenshot) webDriver;
+                File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+                FileUtils.copyFile(screenshot, new File(SCREENSHOTS_DIR.concat(testName).concat(".png")));
+            } catch (IOException exception) {
+                System.out.println("Unable to create a screenshot file for test " + testName + ". Error message: " +
+                        exception.getMessage());
+            }
+        }
+    }
+
+    private void cleanDirectory(String directoryPath) throws IOException {
+        File directory = new File(directoryPath);
+        Assert.assertTrue(directory.isDirectory(), "Invalid directory!");
+
+        try {
+            FileUtils.cleanDirectory(directory);
+            System.out.printf("All files are deleted in Directory: %s%n", directoryPath);
+        } catch (IOException exception) {
+                System.out.printf("Unable to delete the files in Directory: %s%n", directoryPath);
+        }
     }
 }
